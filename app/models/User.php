952f867e0922e72b45581ce7ac3564a1ab288751
Felipe Brigagão de Almeida <felipe-almeida2012@hotmail.com>
2017-08-05 20:17:05 -0300

    Rename  para

--- app/models/User.php
+++ app/models/User.php
@@ -2,9 +2,46 @@
 
 class User extends \HXPHP\System\Model
 {
+	static $validates_presence_of = array(
+		array(
+			'name',
+			'message' => 'O nome é um campo obrigatório.'
+			),
+			array(
+				'email',
+				'message' => 'O E-mail é um campo obrigatório.'
+			),
+			array(
+				'username',
+				'message' => 'O usuário é um campo obrigatório.'
+			),
+			array(
+				'password',
+				'message' => 'A senha é um campo obrigatório.'
+			)
+		);
+
+	   static $validates_uniqueness_of = array(
+       	array(
+       		array('username', 'email'), 
+       		'message' => 'Já existe um usuário com este e-mail e/ou Nome de usuário cadatrado.'
+       		)
+		 );
+
+
 	public static function cadastrar(array $post)
 	{
+		$callbackObj = new \stdClass;
+		$callbackObj->user = null;
+		$callbackObj->status = false;
+		$callbackObj->errors = array();
+
 		$role = Role::find_by_role('user');
+
+		if (is_null($role)) {
+			array_push($callbackObj->errors, 'A role user não existe. COntate o administrador');
+			return $callbackObj;
+		}
 
 		$post = array_merge($post, array(
 			'role_id' => $role->id,
@@ -15,6 +52,20 @@ class User extends \HXPHP\System\Model
 
 		$post = array_merge($post, $password);
 
-		return self::create($post);
+		$cadastrar = self::create($post);
+
+		if ($cadastrar->is_valid()) {
+			$callbackObj->user = $cadastrar;
+			$callbackObj->status = true;
+			return $callbackObj;
+		}
+
+		$errors = $cadastrar->errors->get_raw_errors();
+
+		foreach ($errors as $field => $message) {
+			array_push($callbackObj->errors, $message[0]);
+		}
+
+		return $callbackObj;
 	}
 }
