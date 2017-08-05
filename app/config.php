3fff30babe060b4d074636b793e278f171e84332
Felipe Brigagão de Almeida <felipe-almeida2012@hotmail.com>
2017-08-05 11:34:21 -0300

    Configuração do HXPHP Framework

--- app/config.php
+++ app/config.php
@@ -4,6 +4,13 @@ $configs = new HXPHP\System\Configs\Config;
 ActiveRecord\Connection::$datetime_format = 'Y-m-d H:i:s';
 
 $configs->env->add('development');
+$configs->env->development->baseURI = "/sistema/";
+$configs->env->development->database->setConnectionData(array(
+    'host' => 'localhost',
+    'user' => 'root',
+    'password' => '',
+    'dbname' => 'sistemafba'
+    ));
 
 
 /*
